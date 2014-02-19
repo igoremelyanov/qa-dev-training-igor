2c2122a77d4304f187ce54ab4e29b7e41dd640c6
iemelyanov <iemelyanov@paybyphone.com>
2014-02-19 12:50:39 -0800

    Chapter3.Exercise3-1 firtst commit

---
+++ Chapter3.Exercises/Exercises.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Chapter3.Exercises
+{
+    [TestFixture]
+    public class Exercises
+    {
+        ////////////////Exercise 3-1
+        
+        //  private DateTime _dateTime;
+        [Test]
+        public void ImplicitCasting()
+        {
+            int i = 12;
+            byte j = 4;
+            Console.Out.WriteLine("int {0} +  byte {1} = total int {2}", i, j, i + j);
+        }
+
+    }
+}
