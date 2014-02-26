4e352d5bb8779097412091c42d9e8dbfe5946592
iemelyanov <iemelyanov@paybyphone.com>
2014-02-25 16:40:06 -0800

    add POSFIX and PREFIX Chapter4

--- Chapter4.Exercises/Exercise4_1.cs
+++ Chapter4.Exercises/Exercise4_1.cs
@@ -15,16 +15,81 @@ namespace Exercises
         [Test]
         public void Var()
         {
-            int x = 25;
-            int y = 6;
+           // int x = 25;
+            
+           // x+= 1;
+            //int result = x++;
+           // Console.WriteLine("result {0}", result);
+           // Assert.AreEqual(27, result , "Meanig result ");
+            //x = x+5000;
+           // x += 1;
+            //Console.WriteLine("summ  x+=1 {0}", x);
 
-            Console.WriteLine("summ x+y {0}", x + y);
-            Assert.AreEqual(30, x+y, "Meanig summ x + y not equal 30 ");
+           // int y = 6;
 
-            //if (x+y != 30)
+            //Console.WriteLine("summ x+y {0}", x + y);
+           //ssert.AreEqual(30, x+y, "Meanig summ x + y not equal 30 ");
+
+            //if (x+y || 30) && (x+y=30)
              //{
               //throw new Exception("Summ x + y return wrong value");
              //} 
+        //else
+            //{
+              //  /throw new Exception("Summ x + y return wrong value");
+                
+           // }
+
+            int original = 10;
+            int result;
+            //result = ++original; //from book
+            //same as
+            result = original+1;
+            original +=1;
+
+            Console.WriteLine("After prefix: {0}, {1}", original, result);
+            // assign then increment
+            
+            //result = original++; //from book
+            //same as
+            result = original;
+            original +=1 ;
+            Console.WriteLine("After postfix: {0}, {1}", original, result);
+
+
+            bool p = true;
+            bool q = false;
+            bool r = true;
+
+            if (p && q) 
+            {
+                Console.WriteLine("TRUE {0}, {1}", p, r);
+            }
+            else
+            {
+               Console.WriteLine("FALSE {0}, {1}", p, r);
+               
+            }
+
+
+            int input=1;
+            string classify;
+            // if-else construction.
+            if (input > 0)
+                classify = "positive";
+            else
+                classify = "negative";
+
+            // ?: conditional operator.
+            classify = (input > 0) ? "positive" : "negative";
+
+            Console.WriteLine("CLASSIFY {0}", classify);
+
+
+           
+
+
+
 
 
         }
